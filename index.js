3834a84bebf7a5e4206708239c0d4a60a9b87714
James Elliott <james@deepsymmetry.org>
2020-01-10 15:27:48 -0600

    Try getting actual tag data.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 try {
     const varName = core.getInput('var-name');
-    core.exportVariable(varName, 'TBD!');
+    let result = '';
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            result += data.toString();
+        }
+    };
+    (async () => {
+        await exec.exec('git describe --abbrev-0');
+        core.exportVariable(varName, result);
+    })().catch(e => {
+        core.setFailed(e.message);
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
