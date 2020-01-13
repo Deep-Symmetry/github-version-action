42ed39ca74aedb70349a0a27266dff56d1974e21
James Elliott <james@deepsymmetry.org>
2020-01-13 14:46:42 -0600

    Restore needed git step, remove logging, test between jobs.

--- dist/index.js
+++ dist/index.js
@@ -55,7 +55,6 @@ const isVersion = (tag) => /^(v(ersion)?)?-?\d+(\.\d+)*(-snapshot)?/i.test(tag);
 try {
     const varName = core.getInput('var-name', { required: true });
     const tagVarName = core.getInput('tag-var-name');
-    console.log(`varName=${varName}, tagVarName=${tagVarName}`);
     let result = '';
     const options = {};
     options.listeners = {
@@ -65,13 +64,9 @@ try {
     };
     (async () => {
         await exec.exec('git', ['tag', '--sort=-taggerdate', '--merged'], options);
-        console.log("result:", result);
         const lines = result.split("\n");
-        console.log("lines:", lines);
         const rawVersion = lines.find(isVersion);
-        console.log("rawVersion:", lines);
         const version = rawVersion.replace(/^v(ersion)?-?/i, "").replace(/snapshot$/i, "Preview");
-        console.log(`rawVersion=${rawVersion}, version=${version}`);
         core.exportVariable(varName, version);
         if (!!tagVarName) {
             core.exportVariable(tagVarName, rawVersion);
